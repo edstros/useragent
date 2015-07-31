4fd2bca8d6affa1389166b0c95898734949e759c
Katy Justiss <katy.justiss@gmail.com>
2015-07-31 10:44:13 -0500

    get request for useragent and added boilerplate for usage module

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-require('chalk');
+var chalk = require('chalk');
 var port = process.env.PORT || 1337;
 require('./lib/server')(port);
 
-console.log('Server running on ' + ('http://localhost: ' + port));
+console.log('server running on' + ' ' + chalk.magenta.bold.underline("http://localhost:" + port));
