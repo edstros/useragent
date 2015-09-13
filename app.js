72d53d2da76c60d1ee8092bc2ff1a19968b7e7bb
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-09-13 12:42:56 -0500

    refactored code, updated readme

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ var chalk = require('chalk');
 var port = process.env.PORT || 1337;
 require('./lib/server')(port);
 
-console.log('Server running on ' + ('http://localhost: ' + port));
+console.log('server running on' + ' ' + chalk.magenta.bold.underline("http://localhost:" + port));
